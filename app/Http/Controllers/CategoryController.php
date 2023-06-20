c27e87dcc4c9b574a48bed004d0d08b17efb764b
saikyo muteki <db4649t2006rsk@gmail.com>
2023-06-20 16:20:54 +0900

    fifth-commit カテゴリー修正機能実装 サイドバーにカテゴリー管理追加

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -28,7 +28,7 @@ class CategoryController extends Controller
 
     public function edit(Category $category)
     {
-        return view('categories.edit', compact('category'));
+        return view('categoriesEdit', compact('category'));
     }
 
     public function update(Request $request, Category $category)
--- resources/views/categoriesEdit.blade.php
+++ resources/views/categoriesEdit.blade.php
@@ -10,21 +10,30 @@
     <div class="row">
         <div class="col-md-3 p-0">
             <h1>SimpleMemo</h1>
-            
+
             <!-- メニューリスト -->
             <ul class="list-group">
               <li class="list-group-item {{ request()->is('top') ? 'active' : '' }}"><a href="/top">MENU</a></li>
               <li class="list-group-item {{ request()->is('list') ? 'active' : '' }}"><a href="/list">お店リスト</a></li>
               <li class="list-group-item {{ request()->is('list/create') ? 'active' : '' }}"><a href="/list/create">新規登録/編集</a></li>
               <li class="list-group-item {{ request()->is('categories') ? 'active' : '' }}"><a href="/categories">カテゴリー管理</a></li>
-            </ul>
+          </ul>
         </div>
         <div class="col-md-9">
             <div id="main-content">
-              <h2>デフォルトのコンテンツ</h2>
-              <p><?php echo date('m月d日'); ?>こんにちは！</p>
-          </div>
+              <h1>カテゴリー編集ページ</h1>
+
+              <form action="{{ route('categories.update', $category->id) }}" method="POST">
+                  @csrf
+                  @method('PUT')
+                  <label for="name">カテゴリー名</label>
+                  <input type="text" id="category_name" name="category_name" value="{{ $category->name }}" required>
+                  <button type="submit">修正</button>
+              </form>
+
+              <a href="{{ route('categories.index') }}">戻る</a>
+            </div>
         </div>
     </div>
-</div>
+  </div>
 </body>
